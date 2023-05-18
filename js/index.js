2e7b2280889eb88d4b0e83b1bbcfb515223aeb75
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-05-18 17:10:57 +0200

    Fatto il css

--- js/index.js
+++ js/index.js
@@ -1,25 +1,37 @@
 //DOMANDE DA CHIEDERE ALL'UTENTE
-let km = prompt('Quanti km vuoi percorrere?');
-let anni = prompt('Quanti anni hai?');
+
+//BOTTONE
+let button = document.getElementById('click');
+button.addEventListener('click', function(){
+    let nome = document.getElementById('nome').value;
+    let eta = document.getElementById('anni').value;
+    let km = document.getElementById('km').value;
+    console.log(nome);
+    document.getElementById('stampa').innerHTML = (nome);
+    document.getElementById('offerta').innerHTML = (eta);
+    let prezzoPieno = km * 0.21;
+    let prezzoTot20 = (prezzoPieno - (20 * prezzoPieno / 100)).toFixed(2);
+    let prezzoTot40 = (prezzoPieno - (40 * prezzoPieno / 100)).toFixed(2);
+    
+    const minorenni = 18;
+    const over = 65;
+    
+    if (anni > over){
+        console.log('COSTO PER GLI OVER',prezzoTot40)
+    }else if (anni < minorenni){
+        console.log('COSTO PER IL MINORENNE', prezzoTot20)
+    }else{
+        console.log('PREZZO PIRENO', prezzoPieno)
+    }
+    
+    document.getElementById('costo').innerHTML = (prezzoPieno, prezzoTot40, prezzoTot20);
+    
+})
 
 //ANNI DEI MINORENNI E DEGLI OVER 65 
-const minorenni = 18;
-const over = 65;
 
 //PREZZO DEL BIGLIETTO PER KM
-let prezzoPieno = km * 0.21;
 
 //OPERAZIONE PER LA SCONTISTICA DEI MINORENNI E DEGLI OVER 65
-let prezzoTot40 = (prezzoPieno - (40 * prezzoPieno / 100)).toFixed(2);
-let prezzoTot20 = (prezzoPieno - (20 * prezzoPieno / 100)).toFixed(2);
 
 //SE E ALTRIMENTI
-if (anni > over){
-    console.log('COSTO PER GLI OVER',prezzoTot40)
-}else if (anni < minorenni){
-    console.log('COSTO PER IL MINORENNE', prezzoTot20)
-}else{
-    console.log('PREZZO PIRENO', prezzoPieno)
-}
-
-console.log (anni, km);
